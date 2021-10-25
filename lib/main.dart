9366b11b40c704427136427798a4a41b543d69b0
Poppy Sari <poppy.sari@rga.com>
2021-10-25 19:33:45 +0700

    :lipstick: layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -8,43 +8,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.teal,
-      appBar: AppBar(
-        title: Text('Layout'),
-        backgroundColor: Colors.black45,
-      ),
-      body: SafeArea(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          verticalDirection: VerticalDirection.down,
-          // mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              child: Text('Container 1'),
-              height: 150.0,
-              width: 150.0,
-              alignment: Alignment.center,
-              color: Colors.blueAccent[200],
-            ),
-            Container(
-              child: Text('Container 2'),
-              height: 100.0,
-              width: 100.0,
-              alignment: Alignment.center,
-              color: Colors.red,
-            ),
-            Expanded(
-              child: Container(
-                child: Text('Container 3'),
-                alignment: Alignment.center,
-                color: Colors.yellow,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text('Layout Challenge'),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100,
+                color: Colors.red,
               ),
-            )
-          ],
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.lightGreen,
+                  ),
+                ],
+              ),
+              Container(
+                width: 100,
+                color: Colors.blue,
+              )
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
